a85b7239136ae15e1da65624a9c94caf17e50863
chika-dueke-eze <64172430+chika-dueke-eze@users.noreply.github.com>
2020-09-08 22:24:07 +0100

    Add files via upload

---
+++ punch0201.py
@@ -0,0 +1,16 @@
+"""
+-take 2 parms
+- n (no of digits), num (factor)
+
+find how many times highest no is disvisble - lower inone
+"""
+
+def factor_count(n, m):
+    small_n = 10**(n-1)
+    big_n = (10**n) - 1
+    poss_vals = (big_n//m)-(small_n//m)
+    return poss_vals
+
+if __name__ == "__main__":
+    print(factor_count(1,4))
+    print(factor_count(2,8))
