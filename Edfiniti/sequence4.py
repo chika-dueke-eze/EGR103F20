07a739006776645a3d01f7449cbc2466adb9ea08
chika-dueke-eze <64172430+chika-dueke-eze@users.noreply.github.com>
2020-09-10 03:20:23 +0100

    Create sequence4.py

---
+++ Edfiniti/sequence4.py
@@ -0,0 +1,7 @@
+phrases = ["My, what a lovely day today is!", \
+     "Have you mastered cooking yet? A tasty treat could be in your future.", \
+     "Have you seen the leaves change color?"]
+     
+for sentence in phrases:
+    no_of_ts = sentence.count('t')
+    print(no_of_ts)
