05e271b935711ba4d7c691f0627f1720afd28949
chika-dueke-eze <64172430+chika-dueke-eze@users.noreply.github.com>
2020-09-11 23:24:16 +0100

    Update sequence6.py

--- Edfiniti/sequence6.py
+++ Edfiniti/sequence6.py
@@ -1,2 +1,2 @@
-shoppingList = ["PS5", "Ferrari", "Mansion", "Rocket", "Cabin", "Idk"]
+shoppingList = ["Ferrari", "Rocket", "Mansion", "Cabin", "Idk", "PS5"]
 shoppingString = " then ".join(shoppingList)            #take note of the whitespaces in string " then "
