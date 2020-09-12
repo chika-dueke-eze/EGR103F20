26a4480b871773402711d9b0bea8affac02c1ea0
chika-dueke-eze <64172430+chika-dueke-eze@users.noreply.github.com>
2020-09-12 03:21:39 +0100

    Create conditional4.py

---
+++ Edfiniti/Conditionals/conditional4.py
@@ -0,0 +1,13 @@
+blood_type = input()
+
+if blood_type == 'O':
+    print('O')
+elif blood_type == 'A':
+    print('O A')
+elif blood_type == 'B':
+    print('O B')
+else:
+    print('O A B AB')
+    
+    
+# be careful with spaces and formatting, Edfiniti is super picky
