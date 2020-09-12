2807b569aa77a0fe5bc1ac5d6e550db6b22fff98
chika-dueke-eze <64172430+chika-dueke-eze@users.noreply.github.com>
2020-09-12 03:05:27 +0100

    Create conditional1.py

---
+++ Edfiniti/Conditionals/conditional1.py
@@ -0,0 +1,6 @@
+user_input = input('Would you like to hear an inspirational quote? (yes/no) ')
+
+if user_input == 'yes':
+    print('Great things never come from comfort zones')
+else:
+    print('OK, maybe next time!')
